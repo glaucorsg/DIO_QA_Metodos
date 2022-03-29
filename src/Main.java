41049ff8b07ef554c5dd5a725387282b5fb31b88
glaucorsg <glaucorsg@gmail.com>
2022-03-29 16:11:23 -0300

    Criação das classes e métodos para o terceiro exercício: - Quadrilátero (usando return)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("\n ==== Utilizando a calculadora de operações básicas ====");
+=======
+        System.out.println(" ==== Utilizando a calculadora de operações básicas ====");
+>>>>>>> c83730e ("Criação das classes e métodos para o primeiro exercício:)
         Operacoes.soma(5,8);
         Operacoes.subtracao(7,2);
         Operacoes.multiplicacao(20,4);
         Operacoes.divisao(35,9);
 
+<<<<<<< HEAD
         System.out.println("\n ==== Utilizando o método de cumprimento ====");
+=======
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println(" ==== Utilizando o método de cumprimento ====");
+>>>>>>> c83730e ("Criação das classes e métodos para o primeiro exercício:)
         Cumprimento.cumprimento(19);
         Cumprimento.cumprimento(7);
         Cumprimento.cumprimento(17);
 
+<<<<<<< HEAD
         System.out.println("\n ==== Utilizando o método de cálculo do valor de empréstimo ====");
         Emprestimo.calculoEmprestimo(5,0.055,2000);
         Emprestimo.calculoEmprestimo(15,0.025,25000);
@@ -23,5 +35,11 @@ public class Main {
         Quadrilatero.area(3f,7f); // passando dois floats como parâmetros
 
 
+=======
+        System.out.println(" ==== Utilizando o método de cálculo do valor de empréstimo ====");
+        Emprestimo.calculoEmprestimo(5,0.055,2000);
+        Emprestimo.calculoEmprestimo(15,0.025,25000);
+
+>>>>>>> c83730e ("Criação das classes e métodos para o primeiro exercício:)
     }
 }
